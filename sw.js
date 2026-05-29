1f40fcf943d6dce67978c87c52cb369a9642d2a0
AOK-ai97 <ozodbekadilov092@gmail.com>
2026-05-29 09:42:27 +0500

    sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // AOK PWA Service Worker — avtomatik yangilanish
 // Har deploy da CACHE_VERSION ni o'zgartiring (yoki sana qo'ying)
-const CACHE_VERSION = 'aok-v3-' + '2026-05-25-11';
+const CACHE_VERSION = 'aok-v3-' + '2026-05-25-12';
 const ASSETS = [
   './',
   './index.html',
